32cf705dfc2cd05e7167a688a2a2a2f968ab96e5
xun8szh_t <external.enqiang.xu@cn.bosch.com>
2024-11-29 15:35:45 +0800

    feat: add datajson

---
+++ src/views/data.js
@@ -0,0 +1,22 @@
+/* eslint-disable */
+
+export const rooms = [
+  {
+    name: '客厅',
+    key: 'living-room',
+    map: new URL('@/assets/images/map/map_living_room.jpg', import.meta.url).href,
+    showSwitch: true,
+  },
+  {
+    name: '卧室',
+    key: 'bed-room',
+    map: new URL('@/assets/images/map/map_bed_room.jpg', import.meta.url).href,
+    showSwitch: true,
+  },
+  {
+    name: '书房',
+    key: 'study-room',
+    map: new URL('@/assets/images/map/map_study_room.jpg', import.meta.url).href,
+    showSwitch: true,
+  },
+];
